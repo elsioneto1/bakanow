0cdd06441893568b834be0247f24500587b6e232
elsioneto1 <elsioneto@gmail.com>
2017-01-21 17:37:14 -0500

    RAIO LEIZER

---
+++ Assets/Scripts/Shaders/Frequency.shader
@@ -0,0 +1,77 @@
+﻿Shader "Custom/Frequency" {
+	Properties {
+		_ColorA ("Color A", Color) = (1,1,1,1)
+		_ColorB ("Color B", Color) = (1,1,1,1)
+		_Speed("Wave Speed", Range(0.1,80)) = 5
+		_Frequency("Wave Frequency", Range(0,5)) = 2 
+		_Amplitude("Wave Amplitude", Range (-1,1)) =1
+		_MainTex ("Albedo (RGB)", 2D) = "white" 
+
+	}
+	SubShader 
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		PASS
+		{
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma vertex vert
+		#pragma fragment frag
+		#pragma multi_compile_fog
+
+		#include "UnityCG.cginc"
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+		float4 _ColorA;
+		float4 _ColorB;
+		float _tintAmount;
+		float _Speed;
+		float _Frequency;
+		float _Amplitude;
+		float _OffsetVal;
+
+
+		
+        struct appdata
+        {
+            float4 vertex : POSITION;
+            float2 uv : TEXCOORD0;
+        };
+
+        struct v2f
+        {
+            float2 uv : TEXCOORD0;
+            UNITY_FOG_COORDS(1)
+            float4 vertex : SV_POSITION;
+        };
+
+
+		v2f vert(appdata v) {
+			v2f o;
+			o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+			float time = _Time * _Speed;
+			float waveValueA = sin(time + v.vertex * _Frequency) * _Amplitude;
+			o.vertex.xyz = float3(v.vertex.x, v.vertex.y + waveValueA,v.vertex.z);
+			return o;
+		}
+
+			
+            
+        fixed4 frag (v2f i) : SV_Target
+        {
+            // sample the texture
+            fixed4 col = tex2D(_MainTex, i.uv);
+            // apply fog
+            UNITY_APPLY_FOG(i.fogCoord, col);
+            return col;
+        }
+
+		ENDCG
+		}
+	}
+}
+	
--- Assets/Scripts/Weapon/Ray.cs
+++ Assets/Scripts/Weapon/Ray.cs
@@ -5,50 +5,103 @@ using System.Collections.Generic;
 
 public class Ray : MonoBehaviour {
 
+    Transform cachedTransform;
+
     LineRenderer lineRenderer;
 
     public float LineDrawSpeed;
     Vector3 StartingPoint;
 
+
+
     public List<Point> points = new List<Point>();
+    public Point LatestPoint;
+
+    
+
 
     public float Frequency = 120;
 
-    public Point LatestPoint;
+
+    public bool firing;
+    public float fireRate  = 0.5f;
+    float lastShot;
 
 	// Use this for initialization
 	void Start () {
 
         lineRenderer = GetComponent<LineRenderer>();
-        
-
+        cachedTransform = transform;
+        lineRenderer.sortingLayerName = "OnTop";
+        lineRenderer.sortingOrder = 5;
+        lineRenderer.SetVertexCount(200);
+         for (int i = 0; i <200; i++)
+         {
+             lineRenderer.SetPosition(i, Vector3.zero);
+         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        for (int i = 0; i < points.Count; i++)
+
+        if (points != null)
         {
-                
+            
+            for (int i = 0; i < points.Count; i++)
+            {
+                points[i].MyPosition += points[i].Direction * Time.deltaTime * LineDrawSpeed ;
+                lineRenderer.SetPosition(i, points[i].MyPosition);
+            }
+
+            if (firing)
+            {
+                if (lastShot < 0)
+                {
+
+                    SpawnPoint();
+                    lastShot = fireRate;
+                }
+
+                lastShot -= Time.deltaTime;
+
+
+            }
+        }
+        else
+        {
+         //   points = null;
         }
 
-        
+
+
 
 	}
 
 
-    public void SpawnSpoint()
+    public void SpawnPoint()
     {
-
+        points.Add(new Point(cachedTransform.position,points.Count == 0? null : points[points.Count -1], cachedTransform.right ));
     }
 
 }
 
+[System.Serializable]
 public class Point
 {
     public float DistanceTraveled;
     public Vector3 SpawnPosition;
     public Point PointSpawned;
     public Vector3 Direction;
+    public Vector3 MyPosition;
+
+    public Point(Vector3 spawnPosition, Point pointSpawned, Vector3 direction)
+    {
+
+        SpawnPosition = spawnPosition;
+        PointSpawned = pointSpawned;
+        Direction = direction;
+
+    }
 
 }
